2dec6d46ac87efd38dd1394edfa6adde76045b42
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-06 10:52:07 -0400

    criação das migrates

--- database/migrations/2017_10_06_124332_create_persons_table.php
+++ database/migrations/2017_10_06_124332_create_persons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePersonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('persons', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('registration_number')->nullable();
-            $table->rememberToken();
+            $table->string('cpf');
+            $table->string('rg');
+            
+            $table->integer('organization_id')->unsigned(); 
+            
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('persons');
     }
 }
--- database/migrations/2017_10_06_142936_create_documents_table.php
+++ database/migrations/2017_10_06_142936_create_documents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDocumentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('documents', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('registration_number')->nullable();
-            $table->rememberToken();
+            $table->string('url');
+            $table->string('document_number');
+            $table->date('expiration_date');
+
+            $table->string('document_type_id')->unsigned();
+            
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('documents');
     }
 }
