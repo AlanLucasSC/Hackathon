94662054855e1d5f59f15ea5fdcb6b6329602e20
Diego Tsuyoshi <dttsuyoshi@gmail.com>
2017-10-06 21:48:34 -0400

    chuPA

--- resources/views/organizations/show.blade.php
+++ resources/views/organizations/show.blade.php
@@ -10,6 +10,7 @@
         </tr>
         @foreach ($documents as $document)
           <tr>
+          //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
             <td>{{ $document->url }}</td>
             <td>{{ $document->organization->name }}</td>
           </tr>
